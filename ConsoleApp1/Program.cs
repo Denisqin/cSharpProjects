c1430ce1c054cc2d9a9f8948a87579cfdddc84c4
Denisqin <deniskin9803@gmail.com>
2017-07-06 23:17:11 +0300

    06.07.2017 23:16 added cycles while and for

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //#region BINGO BONGO JIMBO JAMBO
-            //Console.ForegroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Green;
             //#endregion
             //#region Условные конструкции
             //if (DateTime.Now.Hour < 18)
@@ -24,12 +24,28 @@ namespace ConsoleApp1
             //}
             //Console.ReadLine();
             //#endregion
-            switch(DateTime.Now.Hour)
-            {
-                case 21: Console.WriteLine("it's coding time after 21 o'clock"); break;
-                case 23: Console.WriteLine("sleeping time for relaxation and continuation coding"); break;
-            }
-            Console.ReadLine();
+            //#region switch
+            //switch (DateTime.Now.Hour)
+            //{
+            //    case 21: Console.WriteLine("it's coding time after 21 o'clock"); break;
+            //    case 23: Console.WriteLine("sleeping time for relaxation and continuation coding"); break;
+            //}
+            //Console.ReadLine();
+            //#endregion
+            //#region cycles
+            //While
+            //while (DateTime.Now.Minute < 11)
+            //{
+            //    Console.WriteLine(DateTime.Now);
+            //}
+            //Console.ReadLine();
+            //for
+            //for (string i = "w"; i.Length <= 4; i = i + "w")
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadLine();
+            //#endregion
         }
     }
 }
