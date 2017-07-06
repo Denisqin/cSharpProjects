6ddde1c446be703629bb8abf0f1eefff2f9b8e61
Denisqin <deniskin9803@gmail.com>
2017-07-06 20:31:49 +0300

    '06.07.2017FirstCommit'

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //#region BINGO BONGO JIMBO JAMBO
+            //Console.ForegroundColor = ConsoleColor.Green;
+            //#endregion
+            #region Условные конструкции
+            if (DateTime.Now.Hour < 18)
+            {
+                Console.WriteLine("Добрый вечер, время:" + DateTime.Now);
+            }
+            else
+            {
+                Console.WriteLine("Время позднее, пора идти ехай");
+            }
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
