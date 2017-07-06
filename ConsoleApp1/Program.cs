19d549bc00e0dca848940b5441850670c8a065cd
Denisqin <deniskin9803@gmail.com>
2017-07-06 21:30:14 +0300

    06.07.2017 21:29; addedSwitchConstruction

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,17 +13,23 @@ namespace ConsoleApp1
             //#region BINGO BONGO JIMBO JAMBO
             //Console.ForegroundColor = ConsoleColor.Green;
             //#endregion
-            #region Условные конструкции
-            if (DateTime.Now.Hour < 18)
+            //#region Условные конструкции
+            //if (DateTime.Now.Hour < 18)
+            //{
+            //    Console.WriteLine("Добрый вечер, время:" + DateTime.Now);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Время позднее, пора идти ехай!");
+            //}
+            //Console.ReadLine();
+            //#endregion
+            switch(DateTime.Now.Hour)
             {
-                Console.WriteLine("Добрый вечер, время:" + DateTime.Now);
-            }
-            else
-            {
-                Console.WriteLine("Время позднее, пора идти ехай!");
+                case 21: Console.WriteLine("it's coding time after 21 o'clock"); break;
+                case 23: Console.WriteLine("sleeping time for relaxation and continuation coding"); break;
             }
             Console.ReadLine();
-            #endregion
         }
     }
 }
